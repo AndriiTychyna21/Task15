aa41dd69625b103094ca62a691c78e2468d23f60
andrii <andriitychyna2104@gmail.com>
2024-03-05 00:48:49 +0200

    rename method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static double ProductOfArraysElements(double[] array){       //The method that calculates the product
+    public static double productOfArraysElements(double[] array){       //The method that calculates the product
         double result = 1;
         for (double element : array){
             result *= element;
@@ -8,11 +8,11 @@ public class Main {
     }
     public static void main(String[] args) {
         double[] array = new double[10];
-        System.out.print("Our array: ");
+        System.out.print("Your array: ");
         for (int i = 0; i < array.length; i++){
             array[i]=20*Math.random() - 10;              //number generation from -10 to 10
             System.out.print(array[i]+ " ");
         }
-        System.out.println("Result: " + ProductOfArraysElements(array));
+        System.out.println("Result: " + productOfArraysElements(array));
     }
 }
