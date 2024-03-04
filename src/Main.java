b03e70444dff29c964e2cab8d4d2578617ef7e3c
andrii <andriitychyna2104@gmail.com>
2024-03-04 21:45:01 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static double ProductOfArraysElements(double[] array){       //The method that calculates the product
+        double result = 1;
+        for (double element : array){
+            result *= element;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        double[] array = new double[10];
+        System.out.print("Our array: ");
+        for (int i = 0; i < array.length; i++){
+            array[i]=20*Math.random() - 10;              //number generation from -10 to 10
+            System.out.print(array[i]+ " ");
+        }
+        System.out.println("Result: " + ProductOfArraysElements(array));
+    }
+}
